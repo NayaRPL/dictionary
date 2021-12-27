f7d7613e5044b58113b187a570d343bae1b7f2f5
NayaRPL <naya.mmj@gmail.com>
2021-12-27 21:53:39 +0800

    harus

---
+++ data dictionary.py
@@ -0,0 +1,10 @@
+print("data dictionary")
+nama={1:'naya',2:'awal',3:'angung',4:'mita',5:'kembar',6:'lisa'}
+print(nama[2])
+print(nama.get(4))
+regu={"melati":"salsa","mawar":"saitun","asoka":"safika"}
+print(regu["melati"])
+print(regu.get("asoka"))
+print(nama.keys())#untuk informasi tentang daftar kunci
+print(nama.values())
+
